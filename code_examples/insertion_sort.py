fff540e142942c9a824af4d129e61b89e077bbbe
clbokea <clbo@kea.dk>
2019-04-09 10:39:16 +0200

    pdf

--- code_examples/insertion_sort.py
+++ code_examples/insertion_sort.py
@@ -4,23 +4,21 @@ def insertion_sort(l):
 
     for i in range(1, len(l)): 
 
-        key = l[i] 
-                   
-        # Move elements of l[0..i-1], that are 
-        # greater than key, to one position ahead 
-        # of their current position 
-        j = i-1 # 1) = 0
-                # 2) = 1
-        while j >= 0 and key < l[j] : 
-                l[j + 1] = l[j] # swap l[1] with l[0]
-                j -= 1 # = -1
-        l[j + 1] = key # l[0] = 11
+        key = l[i] # l[3]  = 5
+
+        j = i-1 # 2
+        
+        while j >= 0 and key < l[j]: # 5 < 13
+                l[j + 1] = l[j] 
+                j -= 1 # j=1
+        l[j + 1] = key 
   
   
 # Driver code to test above 
-l = [12, 11, 13, 5, 6] 
-# 2) l = [11, 12, 13, 5, 6]
+#l = [12, 11, 13, 5, 6] '
+l = [11, 12, 13, 5, 6, 5, 7 ,10, 44, 66, 77, 2, 3, 888, 9, 6,53495346875 ] 
 insertion_sort(l) 
-
 for i in range(len(l)): 
-    print (f'{l[i]}') 
+    print (f'{l[i]}', end=' ') 
+
+print()
--- exercises/list_of_ints.py
+++ exercises/list_of_ints.py
@@ -6,6 +6,20 @@ generate lists and test the algorithms we worked and will work with
 """
 import sys
 import random
+import time
+
+def is_sorted(l):
+    for i in range(len(l)-1):
+        if l[i] >= l[i+1]:
+            return False
+    return True
+
+
+def bogo_sort(l):
+    while not is_sorted(l):
+        random.shuffle(l)
+    
+    print(l)
 
 
 def list_of_ints(num):
@@ -13,18 +27,55 @@ def list_of_ints(num):
         Takes a number parameter
         Returns: lists of unordered numbers where all numbers from 0-num are represented """
 
-    # Exercise: Generate a list of length len(num) containing all numbers from 0 - len(num)-1
-    # The numbers should NOT be in order 0, 1, 2, 3, 4 etc. It should be an unordered list.
-    # You can use the random module, but not the method "shuffle" from that module.
-    # You should instead make your own "shuffle"
+    list = []
+    for i in range(num):
+        list.append(i)
 
-    pass
+    for i in range(1, len(list)):
+        temp_1 = random.randint(0, i)
+        temp_2 = random.randint(0, i)
+        list[temp_1], list[temp_2] = list[temp_2], list[temp_1]
+
+    return list
+
+
+def list_of_tobias(num):
+    list = []
+    i = 0
+    while i < num:
+        rand = random.randint(1, num)
+        if rand not in list:
+            list.append(rand)
+            i += 1
+
+    return list
 
 
 def main():
+    
+    
+    bogo_sort([2, 3, 1, 10, 33, 22, 66, 44, 11, 22, 33])
+    
+    
     try:
+        # ===  2 times for loops
+        start = time.time()
         lst = list_of_ints(int(sys.argv[1]))
-        print(lst)
+        end = time.time()
+        #print(lst)
+        print('The list_of_ints execusion time was',
+              format(end - start, '.6f'), 'seconds')
+
+        # ===  while loop
+        start = time.time()
+        lst = list_of_tobias(int(sys.argv[1]))
+        end = time.time()
+        #print(lst)
+        print('The list_of_tobias execusion time was',
+              format(end - start, '.6f'), 'seconds')
+
+
+
     except IndexError:
         print('You need to specify the an argument (length of the list to be generated)')
         print('ie. ($ python list_of_ints.py 100)')
